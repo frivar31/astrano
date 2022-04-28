96cf4d684a4ca8a488e4123861c02421b2cd9f4f
adambenaceur <benaceur.adam@gmail.com>
2022-04-28 16:28:43 -0600

    improved header section and started a hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Hero/>
       <Footer/>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Header.scss'
 
 function Header() {
   return (
-    <div>
-        
+    <div className='header'> 
+        <a href='#'> <img src='assets/Astrano-logo.png' alt='' className='header-logo' /> </a>
+
+        <div className='nav-links'>
+          <ul className='nav-bar'> 
+            <li> <a className='active' href='index.html'>Home</a></li>
+            <li> <a href='about.html'>About</a></li>
+            <li> <a href='contact.html'>Contact</a></li>
+            <li> <a href='donate.html'>Donate</a></li>
+          </ul>
+        </div>
     </div>
   )
 }
