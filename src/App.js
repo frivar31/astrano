5e975faa69e9883bb7530cb17ce72519c4d59b99
adambenaceur <benaceur.adam@gmail.com>
2022-05-01 18:23:35 -0600

    added global font Merriweather and added a banner section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Banner from './components/footer-banner/Banner';
+import Banner from './components/banner/Banner';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Banner.scss'
+
+function Banner() {
+  return (
+    <div className='banner-img'>
+      
+      <img src='assets/footer_banner.png'/>
+    
+    </div>
+  )
+}
+
+export default Banner
