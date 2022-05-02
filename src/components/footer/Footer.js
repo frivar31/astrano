fb84b70cf4ebbe0db774989d3b693224a91073ca
adambenaceur <benaceur.adam@gmail.com>
2022-05-02 12:11:41 -0600

    improved footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import './Footer.scss'
 
 function Footer() {
   return (
-    <div>Footer</div>
+
+      <div className="footer">
+        <div className="footer-section-1" >
+            <img className="footer-img" src='assets/new-astrano-logo.png'/>
+              <p>We are not just another training program. We help you connect to people, talk about experiences and it's all about being human.</p>
+              <p>Copyright ©2022. All Rights Reserved.</p>
+          </div>
+          <div className="footer-section-2" >
+            <h5>Products</h5>
+              <p>Mobile app</p>
+              <p>Community</p>
+              <p>Pricing</p>
+              <p>Try for free</p>
+              <p>Careers</p>
+          </div>
+          <div className="footer-section-3" >
+            <h5>About us</h5>
+              <p>Help</p>
+              <p>Drop us a line</p>
+              <p>Read our story</p>
+              <p>Our Blog</p>
+              <p>Features</p>
+          </div>
+          <div className="footer-section-4" >
+            <h5>Get early access to astrano.</h5>
+              <p>Be a part of the movement.</p>
+              <p>Join our newsletter for updates! We promise we won't spam you :)</p>
+              <form className='footer-form'>
+                <input placeholder='email'/>
+                <button>Sign me up</button>
+              </form>
+            <div className='media'>
+              <img src='assets/instagram.svg'/>
+              <img src='assets/facebook.svg'/>
+              <img src='assets/twitter.svg'/>
+            </div>
+              
+          </div>
+          
+      </div>
+ 
+    
+    
   )
 }
 
