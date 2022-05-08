430fb1065f526eb7824bf029e055f3d9cf8d5269
adambenaceur <benaceur.adam@gmail.com>
2022-05-08 17:46:39 -0600

    added a training page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './pages/Contact';
 import Donate from './pages/Donate';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Training from './pages/Training';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
               <Route  path="/contact" element={<><Header/><Contact/></>} />
               <Route  path="/about" element={<><Header/><About/> </> }/>
               <Route  path="/donate" element={<><Header/><Donate/></> } />
+              <Route  path="/training" element={<Training/> } />
           </Routes>
       <Footer/>  
 
---
+++ src/pages/Training.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header/Header'
+
+function Training() {
+  return (
+    <div>
+        <Header/>
+        Training
+    </div>
+  )
+}
+
+export default Training
